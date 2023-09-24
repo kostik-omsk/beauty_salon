e5dcbbc20b1e5b3df310978367d8d1ae716a328e
sidorenko konstantin <kosiak5587@gmail.com>
2023-09-24 15:30:26 +0600

    refactoring: footer markup

--- src/layout/ui/Footer/AppFooter.vue
+++ src/layout/ui/Footer/AppFooter.vue
@@ -1,44 +1,36 @@
 <script lang="ts" setup></script>
 
 <template>
-  <footer class="footer" id="footer">
-    <div class="container-fluid">
-      <div class="row my-1 footer--border">
-        <div class="col-lg-3 m-auto">
-          <div class="footer__logo">
-            <img src="@/assets/svg/logo.svg" alt="logo" height="55" />
-          </div>
-        </div>
-        <div class="col-lg-3 m-auto">
-          <h4>Тел:</h4>
-          <a class="footer__tel" href="tel:+79133851386">
-            <i class="bi bi-telephone"></i>
-            <strong class="ms-1 text-nowrap">+7 (913) 385-13-86</strong>
-          </a>
-        </div>
-        <div class="col-lg-3 m-auto">
-          <div class="m-0">
-            <h4>Адрес:</h4>
-            <span class="text text-nowrap"><i class="bi bi-geo-alt me-1"></i>Кольцово, Технопарковая 1</span>
-          </div>
-        </div>
-        <div class="col-lg-3 m-auto">
-          <div class="footer__socials float-lg-end">
-            <a href="https://www.instagram.com/anastasiya__dankina/" target="_blank"><i class="bi bi-instagram"></i></a>
-            <a href="https://www.t.me/chloe_dankina" target="_blank"><i class="bi bi-telegram"></i></a>
-            <a href="https://api.whatsapp.com/send?phone=79133851386" target="_blank"><i class="bi bi-whatsapp"></i></a>
-          </div>
+  <footer class="container-fluid footer" id="footer">
+    <div class="row my-2 footer__info align-items-center">
+      <div class="col-lg-3">
+        <img src="@/assets/svg/logo.svg" alt="logo" height="55" />
+      </div>
+      <div class="col-lg-3">
+        <h4>Тел:</h4>
+        <a class="footer__tel" href="tel:+79133851386">
+          <i class="bi bi-telephone"></i>
+          <strong class="ms-1 text-nowrap">+7 (913) 385-13-86</strong>
+        </a>
+      </div>
+      <div class="col-lg-3">
+        <h4>Адрес:</h4>
+        <span class="text text-nowrap"><i class="bi bi-geo-alt me-1"></i>Кольцово, Технопарковая 1</span>
+      </div>
+      <div class="col-lg-3">
+        <div class="footer__socials float-lg-end">
+          <a href="https://www.instagram.com/anastasiya__dankina/" target="_blank"><i class="bi bi-instagram"></i></a>
+          <a href="https://www.t.me/chloe_dankina" target="_blank"><i class="bi bi-telegram"></i></a>
+          <a href="https://api.whatsapp.com/send?phone=79133851386" target="_blank"><i class="bi bi-whatsapp"></i></a>
         </div>
       </div>
-      <div class="row">
-        <div class="col">
-          <iframe
-            class="yandex-map"
-            src="https://yandex.ru/map-widget/v1/?um=constructor%3A59ab304d48a7c4a9d8164b23ac331167de9a2f6e721908746653bbda01e64970&amp;source=constructor"
-            frameborder="1"
-          ></iframe>
-        </div>
-      </div>
+    </div>
+    <div class="mt-2 w-100">
+      <iframe
+        class="yandex-map"
+        src="https://yandex.ru/map-widget/v1/?um=constructor%3A59ab304d48a7c4a9d8164b23ac331167de9a2f6e721908746653bbda01e64970&amp;source=constructor"
+        frameborder="1"
+      ></iframe>
     </div>
   </footer>
 </template>
@@ -49,9 +41,26 @@
   background: $myprimary;
   width: 100%;
 
+  @media only screen and (max-width: 992px) {
+    display: flex;
+    gap: 20px;
+  }
+
+  @media only screen and (max-width: 626px) {
+    flex-direction: column;
+  }
+  &__info {
+    @media only screen and (max-width: 992px) {
+      max-width: 300px;
+    }
+    @media only screen and (max-width: 626px) {
+      height: 280px;
+    }
+  }
+
   &__socials {
     a {
-      font-size: 24px;
+      font-size: 32px;
       color: $mygreen;
       margin-left: 20px;
       &:first-child {
