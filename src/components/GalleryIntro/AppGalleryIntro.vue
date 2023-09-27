5dd292b24c571098b529b0401db3af3f92005ea4
sidorenko konstantin <kosiak5587@gmail.com>
2023-09-27 11:31:03 +0600

    fix: image path for dist

--- src/components/GalleryIntro/AppGalleryIntro.vue
+++ src/components/GalleryIntro/AppGalleryIntro.vue
@@ -10,88 +10,15 @@
 import { gsap } from 'gsap'
 import { onMounted } from 'vue'
 import useImageUrl from '@/composable/getImgUrl'
+import { photeBoxes } from './dataGallery'
 
 interface PhotoBoxElement extends HTMLElement {
   tl: gsap.core.Timeline
 }
+
 export default {
   name: 'AppGallery',
   setup() {
-    const photeBoxes = [
-      {
-        id: 'b',
-        column: 0,
-        class: 'photoBox pb-col0',
-        name: '0.webp'
-      },
-      {
-        id: 'b',
-        column: 0,
-        class: 'photoBox pb-col0',
-        name: '1.webp'
-      },
-      {
-        id: 'b',
-        column: 0,
-        class: 'photoBox pb-col0',
-        name: '2.webp'
-      },
-      {
-        id: 'b',
-        column: 0,
-        class: 'photoBox pb-col0',
-        name: '3.webp'
-      },
-      {
-        id: 'b',
-        column: 1,
-        class: 'photoBox pb-col1',
-        name: '4.webp'
-      },
-      {
-        id: 'b',
-        column: 1,
-        class: 'photoBox pb-col1',
-        name: '5.webp'
-      },
-      {
-        id: 'b',
-        column: 1,
-        class: 'photoBox pb-col1',
-        name: '6.webp'
-      },
-      {
-        id: 'b',
-        column: 1,
-        class: 'photoBox pb-col1',
-        name: '7.webp'
-      },
-      {
-        id: 'b',
-        column: 2,
-        class: 'photoBox pb-col2',
-        name: '8.webp'
-      },
-      {
-        id: 'b',
-        column: 2,
-        class: 'photoBox pb-col2',
-        name: '9.webp'
-      },
-      {
-        id: 'b',
-        column: 2,
-        class: 'photoBox pb-col2',
-        name: '10.webp'
-      },
-      {
-        id: 'b',
-        column: 2,
-        class: 'photoBox pb-col2',
-        name: '11.webp'
-      }
-    ]
-
     onMounted(() => {
       const box = document.querySelectorAll('.photoBox ') as NodeListOf<PhotoBoxElement>
 
@@ -99,7 +26,7 @@ export default {
         const column = photeBoxes[i].column
 
         gsap.set(el, {
-          backgroundImage: `url(${useImageUrl(photeBoxes[i].name).value})`,
+          backgroundImage: `url(${photeBoxes[i].name})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           overflow: 'hidden',
