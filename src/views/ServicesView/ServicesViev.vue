d14fa92ee3acf9ab9cce2b51dfd0ee5916fd2b48
Kostia S <kosiak5587@gmail.com>
2023-12-15 11:40:23 +0600

    feat: add services view

--- src/views/ServicesView/ServicesViev.vue
+++ src/views/ServicesView/ServicesViev.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>services</div>
+  <div class="container">
+    <Breadcrumb />
+    <div class="services">
+      <aside class="menu-scroll">
+        <Sidebar />
+      </aside>
+      <section class="service">
+        <RouterView />
+      </section>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Sidebar from '@/components/Sidebar/AppSidebar.vue'
+import Breadcrumb from '@/components/Breadcrumb/AppBreadcrumb.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: 280px auto;
+  gap: 1.1rem;
+}
+.menu-scroll {
+  flex: 1 0 25%;
+}
+.service {
+  flex: 1 0 75%;
+}
+</style>
