8f9392fcf6b947c8f6959ada50642f6a0cbc35e7
Kostia S <kosiak5587@gmail.com>
2024-05-10 16:59:00 +0600

    feat: add dialog

--- src/components/ServiceInfo/ui/AppPriceList.vue
+++ src/components/ServiceInfo/ui/AppPriceList.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import AppModal from '@/components/Modal/AppModal.vue'
+
 const { priceList, title } = defineProps(['priceList', 'title'])
+
+const isDialogOpen = ref(false)
+const currentItemIndex = ref(-1)
+
+const openDialog = (index: number) => {
+  currentItemIndex.value = index
+  isDialogOpen.value = true
+}
+
+const closeDialog = () => {
+  isDialogOpen.value = false
+}
 </script>
 
 <template>
@@ -8,20 +23,35 @@ const { priceList, title } = defineProps(['priceList', 'title'])
       <div class="prices__header">
         <span class="prices__col">Услуга</span>
         <div class="prices__col">
-          Цена
+          Цена &#8381;
           <span class="price-format">{{ title.includes('/') ? `(${title})` : '' }}</span>
         </div>
       </div>
       <div class="prices__item" v-for="(item, index) in priceList" :key="index">
-        <span class="prices__subtitle">{{ item.name }}</span>
-        <span class="prices__price">{{ item.price }}&#8381;</span>
+        <span class="prices__subtitle">
+          {{ item.name }}
+          <i class="prices__info bi bi-question-circle" v-if="item.info" @click="openDialog(index)"></i>
+        </span>
+        <span class="prices__price">{{ item.price }}</span>
       </div>
     </div>
   </div>
+  <AppModal :isOpen="isDialogOpen" @close-dialog="closeDialog">
+    <template #title>{{ priceList[currentItemIndex].name }}</template>
+    <template #content>{{ priceList[currentItemIndex].info }} </template>
+  </AppModal>
 </template>
 
 <style lang="scss" scoped>
 @import '@/assets/style/var.scss';
+@import '@/assets/style/mixins.scss';
+
+.prices {
+  border-radius: 1rem;
+  overflow: hidden;
+
+  @include boxShadow;
+}
 
 .prices__header {
   display: flex;
@@ -39,7 +69,7 @@ const { priceList, title } = defineProps(['priceList', 'title'])
 
 .prices__col {
   text-align: center;
-  font-size: $font-size-md;
+  font-size: $font-size-base;
   font-weight: 700;
 }
 
@@ -49,7 +79,7 @@ const { priceList, title } = defineProps(['priceList', 'title'])
   gap: 1rem;
   padding: 1rem 0.8rem;
   border-top: 2px solid #d0d0d0;
-  font-size: $font-size-md;
+  font-size: $font-size-base;
 
   &:nth-child(odd) {
     background-color: #eeeeee;
@@ -59,8 +89,18 @@ const { priceList, title } = defineProps(['priceList', 'title'])
   display: block;
 }
 .prices__price {
+  text-align: end;
   flex-grow: 0;
   flex-shrink: 1;
   font-weight: 800;
+}
+
+.prices__info {
+  transition: color 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    color: $mygreen-dark;
+  }
 }
 </style>
