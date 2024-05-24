49b03bed5718a2e7973b3b0c7c7e24616063f5ad
Kostia S <kosiak5587@gmail.com>
2024-05-24 15:28:17 +0600

    feat: upd modal

--- src/components/Modal/AppModal.vue
+++ src/components/Modal/AppModal.vue
@@ -13,8 +13,15 @@ const handleClose = () => {
       <div class="dialog" v-if="props.isOpen" @click="handleClose">
         <div class="dialog-content fade-animation" @click.stop>
           <button type="button" class="close btn-close" @click="handleClose" aria-label="Закрыть"></button>
-          <h4><slot name="title"></slot></h4>
-          <p><slot name="content"></slot></p>
+          <h4 v-if="$slots.title">
+            <slot name="title" />
+          </h4>
+          <p v-if="$slots.content">
+            <slot name="content" />
+          </p>
+          <div v-if="$slots.foto" class="foto">
+            <slot name="foto" />
+          </div>
         </div>
       </div>
     </Transition>
@@ -23,6 +30,7 @@ const handleClose = () => {
 
 <style lang="scss" scoped>
 @import '@/assets/style/mixins.scss';
+
 .fade-enter-active {
   animation: bounce-in 0.3s;
 }
@@ -57,6 +65,8 @@ const handleClose = () => {
   overflow: hidden;
   z-index: 999;
   background-color: rgba(0, 0, 0, 0.3);
+  -webkit-backdrop-filter: blur(8px);
+  backdrop-filter: blur(8px);
 }
 
 .dialog-content {
@@ -65,11 +75,20 @@ const handleClose = () => {
   margin-right: calc(var(--margin-x) * 0.5);
   margin-left: calc(var(--margin-x) * 0.5);
   background-color: #fefefe;
-  padding: 20px;
+  padding: 35px 20px 20px 20px;
+
   border-radius: 1rem;
 }
 
 .close {
-  float: right;
+  position: absolute;
+  top: 9px;
+  right: 9px;
+}
+
+.foto {
+  width: 100%;
+  border-radius: 1rem;
+  overflow: hidden;
 }
 </style>
