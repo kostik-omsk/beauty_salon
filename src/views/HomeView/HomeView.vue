d110f7fcecb09dad5a91d155de66cc71cdd35d6f
Kostia S <kosiak5587@gmail.com>
2024-05-29 16:02:34 +0600

    refactor: add SectionBlock

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -6,89 +6,84 @@ import AppVKPosts from '@/components/VKPosts/AppVKPosts.vue'
 import CardServicesVue from './ui/CardServices.vue'
 import LogoBrands from './ui/LogoBrands.vue'
 import CosmeticsVideo from './ui/CosmeticsVideo.vue'
+import SectionBlock from './ui/SectionBlock.vue'
 
 const ListServices = useServicesStore()
 const { listServices } = storeToRefs(ListServices)
 </script>
 
 <template>
-  <section class="container intro">
-    <div class="intro__block">
-      <h1 class="block-title">Совершенство доступно</h1>
-      <div class="block-text block-text-green">
-        <i class="bi bi-geo-alt"></i>
-        Кольцово, Технопaрковая 1
-      </div>
-      <div class="block-icons mt-1">
-        <a href="https://vk.com/dankina_a_s" target="_blank">
-          <i class="my-icon">
-            <svg class="icon-vk" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
-              <path
-                d="M25,2C12.318,2,2,12.318,2,25s10.318,23,23,23c12.683,0,23-10.318,23-23S37.683,2,25,2z M34.973,29.535 c2.237,1.986,2.702,2.695,2.778,2.816C38.678,33.821,36.723,34,36.723,34h-4.12c0,0-1.003,0.011-1.86-0.557 c-1.397-0.915-2.86-2.689-3.888-2.381C25.992,31.32,26,32.486,26,33.483C26,33.84,25.693,34,25,34s-0.981,0-1.288,0 c-2.257,0-4.706-0.76-7.149-3.313c-3.456-3.609-6.487-10.879-6.487-10.879s-0.179-0.366,0.016-0.589 c0.221-0.25,0.822-0.218,0.822-0.218L14.909,19c0,0,0.376,0.071,0.646,0.261c0.223,0.156,0.347,0.454,0.347,0.454 s0.671,2.216,1.526,3.629c1.67,2.758,2.447,2.828,3.014,2.531C21.27,25.445,21,22.513,21,22.513s0.037-1.259-0.395-1.82 c-0.333-0.434-0.97-0.665-1.248-0.701c-0.225-0.029,0.151-0.423,0.63-0.648C20.627,19.059,21.498,18.986,23,19 c1.169,0.011,1.506,0.081,1.962,0.186C26.341,19.504,26,20.343,26,23.289c0,0.944-0.13,2.271,0.582,2.711 c0.307,0.19,1.359,0.422,3.231-2.618c0.889-1.442,1.596-3.834,1.596-3.834s0.146-0.263,0.373-0.393 c0.232-0.133,0.225-0.13,0.543-0.13S35.832,19,36.532,19c0.699,0,1.355-0.008,1.468,0.402c0.162,0.589-0.516,2.607-2.234,4.797 C32.943,27.793,32.63,27.457,34.973,29.535z"
-              />
-            </svg>
-          </i>
-        </a>
-        <a href="https://www.instagram.com/anastasiya__dankina/" target="_blank"><i class="bi bi-instagram"></i></a>
-        <a href="https://www.t.me/chloe_dankina" target="_blank"><i class="bi bi-telegram"></i></a>
-        <a href="https://api.whatsapp.com/send?phone=79133851386" target="_blank"><i class="bi bi-whatsapp"></i></a>
-      </div>
-    </div>
-    <AppGalleryIntro />
-  </section>
-  <section class="services">
-    <div class="section__header">
-      <div class="container">
-        <h2 class="py-3">Наши услуги</h2>
-      </div>
-    </div>
-    <div class="container">
-      <div class="services__list">
-        <CardServicesVue v-for="service in listServices" :key="service.id" :service="service" />
-      </div>
-    </div>
-  </section>
-  <section class="cosmetics">
-    <div class="section__header">
-      <div class="container">
-        <h2 class="py-3">Косметика</h2>
-      </div>
-    </div>
-    <div class="container">
-      <LogoBrands />
-      <h3>У нас вы сможете приобрести косметику по уходу за лицом и телом от различных брендов</h3>
-      <div class="cosmetics__content">
-        <div class="cosmetics__info">
-          <h4 class="cosmetics__title">О важности использования косметики</h4>
-          <p>
-            Косметика для ухода за лицом и телом - это важный элемент в нашей повседневной жизни. Некоторые продукты
-            могут помочь увлажнить кожу, устранить шелушение и защитить ее от воздействия внешней среды, а также
-            устраняют недостатки и проблемы. Кроме того, использование косметики для ухода может уменьшить выраженность
-            морщин и поддерживать здоровый вид кожи.
-          </p>
-          <p>
-            Важно помнить, что при выборе косметических продуктов необходимо учитывать тип кожи, возраст и
-            индивидуальное состояние кожи, чтобы получить максимальный эффект. В общем, приобретение качественной
-            косметики для ухода за лицом и телом - это важный шаг к здоровью и красоте.
-          </p>
-          <router-link class="primary-btn" to="#">Каталог косметики</router-link>
+  <SectionBlock class="intro container">
+    <template #content>
+      <div class="intro__block">
+        <h1 class="block-title">Совершенство доступно</h1>
+        <div class="block-text block-text-green">
+          <i class="bi bi-geo-alt"></i>
+          Кольцово, Технопaрковая 1
         </div>
-        <div class="cosmetics__video">
-          <CosmeticsVideo />
+        <div class="block-icons mt-1">
+          <a href="https://vk.com/dankina_a_s" target="_blank">
+            <i class="my-icon">
+              <svg class="icon-vk" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
+                <path
+                  d="M25,2C12.318,2,2,12.318,2,25s10.318,23,23,23c12.683,0,23-10.318,23-23S37.683,2,25,2z M34.973,29.535 c2.237,1.986,2.702,2.695,2.778,2.816C38.678,33.821,36.723,34,36.723,34h-4.12c0,0-1.003,0.011-1.86-0.557 c-1.397-0.915-2.86-2.689-3.888-2.381C25.992,31.32,26,32.486,26,33.483C26,33.84,25.693,34,25,34s-0.981,0-1.288,0 c-2.257,0-4.706-0.76-7.149-3.313c-3.456-3.609-6.487-10.879-6.487-10.879s-0.179-0.366,0.016-0.589 c0.221-0.25,0.822-0.218,0.822-0.218L14.909,19c0,0,0.376,0.071,0.646,0.261c0.223,0.156,0.347,0.454,0.347,0.454 s0.671,2.216,1.526,3.629c1.67,2.758,2.447,2.828,3.014,2.531C21.27,25.445,21,22.513,21,22.513s0.037-1.259-0.395-1.82 c-0.333-0.434-0.97-0.665-1.248-0.701c-0.225-0.029,0.151-0.423,0.63-0.648C20.627,19.059,21.498,18.986,23,19 c1.169,0.011,1.506,0.081,1.962,0.186C26.341,19.504,26,20.343,26,23.289c0,0.944-0.13,2.271,0.582,2.711 c0.307,0.19,1.359,0.422,3.231-2.618c0.889-1.442,1.596-3.834,1.596-3.834s0.146-0.263,0.373-0.393 c0.232-0.133,0.225-0.13,0.543-0.13S35.832,19,36.532,19c0.699,0,1.355-0.008,1.468,0.402c0.162,0.589-0.516,2.607-2.234,4.797 C32.943,27.793,32.63,27.457,34.973,29.535z"
+                />
+              </svg>
+            </i>
+          </a>
+          <a href="https://www.instagram.com/anastasiya__dankina/" target="_blank"><i class="bi bi-instagram"></i></a>
+          <a href="https://www.t.me/chloe_dankina" target="_blank"><i class="bi bi-telegram"></i></a>
+          <a href="https://api.whatsapp.com/send?phone=79133851386" target="_blank"><i class="bi bi-whatsapp"></i></a>
         </div>
       </div>
-    </div>
-  </section>
-  <section class="posts">
-    <div class="section__header">
+      <AppGalleryIntro />
+    </template>
+  </SectionBlock>
+  <SectionBlock class="services">
+    <template #title>Наши услуги</template>
+    <template #content>
       <div class="container">
-        <h2 class="py-3">Блог</h2>
+        <div class="services__list">
+          <CardServicesVue v-for="service in listServices" :key="service.id" :service="service" />
+        </div>
       </div>
-    </div>
-    <div class="breakout">
+    </template>
+  </SectionBlock>
+  <SectionBlock class="cosmetics">
+    <template #title>Косметика</template>
+    <template #content>
+      <div class="container">
+        <LogoBrands />
+        <h3>У нас вы сможете приобрести косметику по уходу за лицом и телом от различных брендов</h3>
+        <div class="cosmetics__content">
+          <div class="cosmetics__info">
+            <h4 class="cosmetics__title">О важности использования косметики</h4>
+            <p>
+              Косметика для ухода за лицом и телом - это важный элемент в нашей повседневной жизни. Некоторые продукты
+              могут помочь увлажнить кожу, устранить шелушение и защитить ее от воздействия внешней среды, а также
+              устраняют недостатки и проблемы. Кроме того, использование косметики для ухода может уменьшить
+              выраженность морщин и поддерживать здоровый вид кожи.
+            </p>
+            <p>
+              Важно помнить, что при выборе косметических продуктов необходимо учитывать тип кожи, возраст и
+              индивидуальное состояние кожи, чтобы получить максимальный эффект. В общем, приобретение качественной
+              косметики для ухода за лицом и телом - это важный шаг к здоровью и красоте.
+            </p>
+            <router-link class="primary-btn" to="#">Каталог косметики</router-link>
+          </div>
+          <div class="cosmetics__video">
+            <CosmeticsVideo />
+          </div>
+        </div>
+      </div>
+    </template>
+  </SectionBlock>
+  <SectionBlock class="posts">
+    <template #title>Блог</template>
+    <template #content>
       <AppVKPosts />
-    </div>
-  </section>
+    </template>
+  </SectionBlock>
 </template>
 
 <style lang="scss">
@@ -141,7 +136,13 @@ const { listServices } = storeToRefs(ListServices)
       a {
         font-size: 2rem;
         color: $myprimary;
-        @include myTransitionAll;
+
+        .bi-instagram,
+        .bi-telegram,
+        .bi-whatsapp,
+        .icon-vk {
+          @include myTransitionAll;
+        }
 
         &:hover {
           .my-icon svg {
@@ -203,6 +204,7 @@ const { listServices } = storeToRefs(ListServices)
       align-items: center;
     }
   }
+
   &__video {
     flex: 0 0 32%;
     max-width: 400px;
---
+++ src/views/HomeView/ui/SectionBlock.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <div class="header" v-if="$slots.title">
+      <div class="container">
+        <h2 class="py-3"><slot name="title" /></h2>
+      </div>
+    </div>
+    <slot name="content" />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/style/var.scss';
+.header {
+  color: $secondary;
+  background: $myprimary;
+  margin-bottom: 3rem;
+
+  h2 {
+    font-weight: 800;
+    text-shadow: 0px 0px 10px #000000;
+  }
+}
+</style>
