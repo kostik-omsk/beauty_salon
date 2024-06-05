1e72e52b188cb32a9a8329c4f79a399e6cfea56a
Kostia S <kosiak5587@gmail.com>
2024-06-05 11:34:08 +0600

    feat: upd load posts

--- src/stores/useVkPostsStore.ts
+++ src/stores/useVkPostsStore.ts
@@ -1296,16 +1296,30 @@ export const useVkPostsStore = defineStore({
           count: 259
         }
       }
-    ] as WallWallpostFull[]
+    ] as WallWallpostFull[],
+    offset: 0, // добавляем offset в state
+    isLoading: false, // флаг для предотвращения множественных запросов
+    hasMore: true // флаг, указывающий на наличие дополнительных постов
   }),
   actions: {
     async loadPosts() {
+      if (this.isLoading || !this.hasMore) return
+
+      this.isLoading = true
       try {
-        const response = await fetch('vkpost.php')
+        const response = await fetch(`vkpost.php?offset=${this.offset}`)
         const data = await response.json()
-        this.posts = data.response.items as WallWallpostFull[]
+
+        if (data.response.items.length > 0) {
+          this.posts.push(...(data.response.items as WallWallpostFull[]))
+          this.offset += data.response.items.length
+        } else {
+          this.hasMore = false // если больше нет постов, устанавливаем флаг в false
+        }
       } catch (error) {
         console.error('Error loading VK posts:', error)
+      } finally {
+        this.isLoading = false
       }
     }
   }
