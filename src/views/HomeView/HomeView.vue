2e89ea42773f8b7ee0a7e11a0fb1149723ca07e6
Kostia S <kosiak5587@gmail.com>
2024-06-11 18:43:55 +0600

    refactor: section posts

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,25 +1,15 @@
 <script setup lang="ts">
-import AppVKPosts from '@/components/VKPosts/AppVKPosts.vue'
-
-import SectionBlock from '@/components/SectionBlock/SectionBlock.vue'
 import IntroSection from './IntroSection/IntroSection.vue'
 import ServicesSection from './ServicesSection/ServicesSection.vue'
 import CosmeticsSection from './CosmeticsSection/CosmeticsSection.vue'
+import PostsSection from './PostsSection/PostsSection.vue'
 </script>
 
 <template>
   <IntroSection />
   <ServicesSection />
   <CosmeticsSection />
-  <SectionBlock class="posts">
-    <template #title>Блог</template>
-    <template #content>
-      <AppVKPosts />
-    </template>
-  </SectionBlock>
+  <PostsSection />
 </template>
 
-<style lang="scss">
-@import '@/assets/style/var.scss';
-@import '@/assets/style/mixins.scss';
-</style>
+<style lang="scss"></style>
---
+++ src/views/HomeView/PostsSection/PostsSection.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SectionBlock from '@/components/SectionBlock/SectionBlock.vue'
+import AppVKPosts from './ui/VKPosts/AppVKPosts.vue'
+</script>
+
+<template>
+  <SectionBlock class="posts">
+    <template #title>Блог</template>
+    <template #content>
+      <AppVKPosts />
+    </template>
+  </SectionBlock>
+</template>
+
+<style lang="scss" scoped></style>
